97575c847a8a9155014060c66c280af141c566b4
devmishra888 <devmishra888@gmail.com>
2025-01-08 17:28:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-//your JS code here. If required.
+let inputBoxes = document.querySelectorAll('.code');
 
+        inputBoxes.forEach((input, index) => {
+            //
+            input.addEventListener('input', function() {
+                if (input.value.length == 1 && index < inputBoxes.length - 1) {
+                    inputBoxes[index + 1].focus();  
+                }
+            });
 
-let input = document.querySelectorAll('.code')
-
-for(let i = 0; i < input.length; i++){
-    input[i].addEventListener('input', function(e) {
-        if (e.target.value.length == 1 && i < input.length - 1) {
-            input[i + 1].focus();
-        }
-    });
-}
-
-input[i].addEventListener('keydown', function(e){
-    if(e.key == 'Backspace' && input.value.length === 0 && index > 0){
-    input[i - 1].focus();
- }
-})
+            
+            input.addEventListener('keydown', function(e) {
+                if (e.key === 'Backspace' && input.value.length === 0 && index > 0) {
+                    inputBoxes[index - 1].focus();  
+                }
+            });
+        });
